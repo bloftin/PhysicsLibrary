48f27cdf9f533a576041ead83451f87db6058c06
Benjamin Loftin <ben.loftin@gmail.com>
2025-01-30 22:30:57 -0700

    Added all the entry histories to the repo

---
+++ data/versions/objects/237/images.pl
@@ -0,0 +1,18 @@
+# LaTeX2HTML 2002-2 (1.70)
+# Associate images original text with physical files.
+
+
+$key = q/displaystylefrac{d^2psi(x)}{dx^2}=frac{2m}{hbar^2}left[kx-Eright]psi(x);MSF=1.6;AAT;NTR/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="199" HEIGHT="56" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\displaystyle \frac{d^2\psi(x)}{dx^2}=\frac{2m}{\hbar^2}\left[ kx - E \right] \psi(x)$">|; 
+
+$key = q/V(x)=kx;MSF=1.6;AAT;NTR/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="77" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ V(x) = kx$">|; 
+
+1;
+
